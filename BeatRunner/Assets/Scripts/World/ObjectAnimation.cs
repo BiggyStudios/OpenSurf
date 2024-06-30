7ebc34f872d4b3069cbbf78f5fb5af9194780b00
IckyTheBiggy <elcia116eb@gmail.com>
2024-06-30 14:52:44 +0100

    Fixed nullexception when starting and made the object animations use coroutines

--- BeatRunner/Assets/Scripts/World/ObjectAnimation.cs
+++ BeatRunner/Assets/Scripts/World/ObjectAnimation.cs
@@ -7,9 +7,13 @@ using Random = UnityEngine.Random;
 
 public class ObjectAnimation : MonoBehaviour
 {
+    [SerializeField] private AnimationCurve _animationCurve;
+    
     [SerializeField] private float _loadDistance;
     [SerializeField] private float _positionOfffset;
     [SerializeField] private float _lerpSpeed;
+
+    private bool _animated;
     
     private Vector3 _startPosition;
     private Vector3 _startScale;
@@ -32,18 +36,58 @@ public class ObjectAnimation : MonoBehaviour
 
     private void Update()
     {
+        if (PlayerManager.Instance == null)
+            return;
+        
         float distance = Vector3.Distance(PlayerManager.Instance.PlayerTransform.position, _startPosition);
 
-        if (distance <= _loadDistance)
+        if (distance <= _loadDistance && !_animated)
         {
-            transform.position = Vector3.Lerp(transform.position, _startPosition, Time.deltaTime * _lerpSpeed);
-            transform.localScale = Vector3.Lerp(transform.localScale, _startScale, Time.deltaTime * _lerpSpeed);
+            AnimateObject(true);
+            _animated = true;
         }
-        
-        else
+
+        else if (distance > _loadDistance && _animated)
         {
-            transform.position = Vector3.Lerp(transform.position, _randomPos, Time.deltaTime * _lerpSpeed);
-            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, Time.deltaTime * _lerpSpeed);
+            AnimateObject(false);
+            _animated = false;
         }
+    }
+
+    private void AnimateObject(bool animate)
+    {
+        if (_animateCoroutine != null)
+        {
+            StopCoroutine(_animateCoroutine);
+            _animateCoroutine = StartCoroutine(AnimateRoutine(animate));
+        }
+
+        _animateCoroutine = StartCoroutine(AnimateRoutine(animate));
+    }
+    
+
+    private Coroutine _animateCoroutine;
+    private IEnumerator AnimateRoutine(bool animate)
+    {
+        Vector3 startPos = transform.position;
+        Vector3 startScale = transform.localScale;
+        float lerpPos = 0f;
+
+        while (lerpPos < 1f)
+        {
+            lerpPos = Mathf.Clamp01(Time.deltaTime / _lerpSpeed + lerpPos);
+            float t = _animationCurve.Evaluate(lerpPos);
+
+            transform.position =
+                animate ? Vector3.Lerp(startPos, _startPosition, t) : Vector3.Lerp(startPos, _randomPos, t);
+            
+            transform.localScale = 
+                animate ? Vector3.Lerp(startScale, _startScale, t) : Vector3.Lerp(startScale, Vector3.zero, t);
+
+            yield return null;
+        }
+
+        transform.position = animate ? _startPosition : _randomPos;
+        transform.localScale = animate ? _startScale : Vector3.zero;
     }
 }
