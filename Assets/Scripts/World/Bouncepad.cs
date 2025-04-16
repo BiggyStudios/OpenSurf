fcebe5f77dc4ec16d0f63f17891d9f42886bc664
IckyTheBiggy <elcia116eb@gmail.com>
2025-04-16 10:22:13 +0100

    Bounce pads now launch the player properly

--- Assets/Scripts/World/Bouncepad.cs
+++ Assets/Scripts/World/Bouncepad.cs
@@ -7,7 +7,13 @@ public class Bouncepad : MonoBehaviour
     [SerializeField] private Vector3 _launchForce;
 
     public void LaunchPlayer(PlayerLogic playerLogic)
-    {
-        playerLogic.PlayerData.Velocity += _launchForce;
+    {        
+        Vector3 launchVectorX = playerLogic.transform.right * _launchForce.x;
+        Vector3 launchVectorY = playerLogic.transform.up * _launchForce.y;
+        Vector3 launchVectorZ = playerLogic.transform.forward * _launchForce.z;
+
+        Vector3 launchVector = launchVectorX + launchVectorY + launchVectorZ;
+
+        playerLogic.PlayerData.Velocity += launchVector;
     }
 }
