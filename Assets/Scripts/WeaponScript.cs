f894a90a64121a35ae708ca475f4fa04e0eac7dd
IckyTheBiggy <elcia116eb@gmail.com>
2025-04-18 11:17:52 +0100

    Added some weapon models

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -37,15 +37,11 @@ public class WeaponScript : NetworkBehaviour
         Ray ray = new Ray(PlayerCamera.transform.position, PlayerCamera.transform.forward);
         RaycastHit hit;
 
-        Debug.Log("Shot");
-
         if (Physics.Raycast(ray, out hit, WeaponScriptObj.WeaponRange))
         {
             HealthScript targetHealth = hit.collider.GetComponent<HealthScript>();
             if (targetHealth != null)
             {
-                Debug.Log("Shot hit");
-                //CmdServerDamageTarget(targetHealth.netId, Damage);
                 CmdServerDamageTarget(targetHealth);
             }
         }
@@ -54,23 +50,6 @@ public class WeaponScript : NetworkBehaviour
     [Command]
     private void CmdServerDamageTarget(HealthScript healthScript)
     {
-        Debug.Log("Called server damage RPC");
-
-        /*
-        if (NetworkServer.spawned.TryGetValue(targetNetID, out NetworkIdentity targetIdentity))
-        {
-
-            Debug.Log("Called net-server damage on netID of player");
-            HealthScript targetHealthScript = targetIdentity.GetComponent<HealthScript>();
-
-            if (targetHealthScript != null)
-            {
-                Debug.Log("Actually damaged the player");
-                targetHealthScript.TakeDamageOnServer(damageAmount);
-            }
-        }
-        */
-
         healthScript.TakeDamageOnServer(WeaponScriptObj.Damage);
     }
 }
